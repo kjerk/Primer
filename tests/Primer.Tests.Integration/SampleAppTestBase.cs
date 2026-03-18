523e5c18c3a47ceba2ab23adcebe9efc51efff07
kjerk <kjerk@pm.me>
2026-03-17 23:14:21 -0700

    Add small note about the cross test reach

--- tests/Primer.Tests.Integration/SampleAppTestBase.cs
+++ tests/Primer.Tests.Integration/SampleAppTestBase.cs
@@ -7,12 +7,14 @@ public abstract class SampleAppTestBase : PrimerTestBase<Program>
 {
 	protected override void ConfigureBuilder(WebApplicationBuilder builder)
 	{
+		// Borrowing from Primer.Samples.Standard , any mocked or other impl here.
 		new AppInitWebFeatures().ConfigureBuilder(builder);
 		new AppInitServices().ConfigureBuilder(builder);
 	}
 	
 	protected override void ConfigureApp(WebApplication app)
 	{
+		// Borrowing from Primer.Samples.Standard , any mocked or other impl here.
 		new AppInitWebFeatures().ConfigureApp(app);
 		new AppInitServices().ConfigureApp(app);
 	}
